e82dc2fa7b57cea5de52f251ea3aa990f20b6ad2
Luis Segoviano <luissegoviano.14@gmail.com>
2018-12-17 21:24:39 -0600

    Initial Commit

---
+++ Yuumee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yuumee
+//
+//  Created by Luis Segoviano on 12/17/18.
+//  Copyright © 2018 lsegoviano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
